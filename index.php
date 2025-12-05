1a8612f800e678b4f37462b0567026f16472f6c0
femkegoos <femke.goos.01@gmail.com>
2025-12-05 12:02:17 +0100

    Start site

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="hero">
+    <h1>Welcome to Korean Skincare,</h1> 
+    <h3> You are logged in. You can now add products! You can log out anytime you want!</h3>
+    <br><br>
+    <a href="logout.php" class="btn">Logout</a> 
+        <h1>Welcome to Korean Skincare!</h1> <br>
+      <a href="login.php" class="btn">Log in</a> 
+</div>
+  </body>
+</html>
