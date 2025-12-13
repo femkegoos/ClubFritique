46126020278d2c00e9b8c4d38de58befe48fb613
femkegoos <femke.goos.01@gmail.com>
2025-12-13 16:04:02 +0100

    feat: add footer

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "100%";
+  document.getElementById("mySidepanel").style.width = "100%"; // of 250px, afhankelijk van je ontwerp
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
+
+// Menu links sluiten sidepanel op mobiel
+const menuLinks = document.querySelectorAll('.sidepanel a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    closeNav();
+  });
+});
